eda08d6a52671ecf3b0946fd3ed53ecde6ccdb06
ArmandoGJ19 <armandosannin@gmail.com>
2024-06-05 21:27:18 -0600

    cambios pendientes en context-auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+import {ENV} from "../utils/constants";
 const register = async (name, password, email, lastname) => {
-    return await axios.post('https://evaluacion-2.vercel.app/api/users/', {
+    return await axios.post(`${ENV.API_URL}/${ENV.ENDPOINTS.REGISTER}`, {
         name,
         password,
         email,
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,11 @@
 
 export const ENV = {
+    API_URL: "http://localhost:3000",
+    ENDPOINTS: {
+        LOGIN: "api/auth/signin",
+        REGISTER: "api/users",
+        USER: "api/users",
+    },
     STORAGE: {
         TOKEN:"token"
     }
